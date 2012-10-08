50221d3dac04ab0b4faa58f831937513533cf5d2
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2012-10-08 05:05:08 -0400

    updating b4, use thereof

--- src/d3.b4.js
+++ src/d3.b4.js
@@ -1,36 +1,80 @@
 (function(b4){
     
-    var D3_WIKI = "https://github.com/mbostock/d3/wiki/",
-        D3_TYPES = {
-            SELECTION: {
-                id: "D3 Selection",
-                field: b4.field.text("SELECTOR")
-                    .init("CSS-style selector")
-            }
-        };
+var D3_WIKI = "https://github.com/mbostock/d3/wiki/",
+    D3_TYPES = {
+        SELECTION: {
+            id: "D3 Selection",
+            field: b4.fields.text("SELECTOR")
+                .init("CSS-style selector")
+        },
+        PARENT:{
+            id: "D3 Parent Selection",
+            field: b4.input("PARENT")
+                .title("from")
+                .nextStatement(true)
+        }
+    };
     
 
-    // set up a base configuration
-    var d3_mold = b4.block()
-            .generator("JavaScript")
-            .helpUrlTemplate(D3_WIKI)
-            .namespace("")
-            .colour("steelBlue"),
+// set up a base configuration
+var d3_mold = b4.block()
+    .generator("JavaScript")
+    .helpUrlTemplate(D3_WIKI)
+    .namespace("")
+    .colour("steelBlue"),
             
-        // make a subconfiguration
-        select_mold = d3_mold.clone()
-            .namespace("d3_select")
-            .helpUrlTemplate(D3_WIKI +"Selections#wiki-<%= block.id() %>")
-            .category("d3 Selection");
+    // make a subconfiguration
+    select_mold = d3_mold.clone()
+        .namespace("d3_select")
+        .category("d3 Selection")
+        .output(D3_TYPES.SELECTION)
+        .helpUrlTemplate(D3_WIKI +"Selections#wiki-<%= block.id() %>"),
+    
+    select_arg_mold = select_mold.clone()
+        .namespace("d3_d3_")
+        .output(D3_TYPES.SELECTION);
             
-        select_mold.clone("")
-            .appendTitle("select the first element that matches")
-            .appendTitle(D3_TYPES.SELECTION.field)
-            .tooltip("The first element that matches the selector")
-            .output(D3_TYPES.SELECTION)
-            .code("d3.select('<%= block.getTitleValue('SELECTOR') %>')")
-            .done();
+    select_mold.clone("")
+        .tooltip("The first element that matches the selector")
+        .appendTitle("select the first element that matches")
+        .appendTitle(D3_TYPES.SELECTION.field)
+        .code("d3.select('<%= block.getTitleValue('SELECTOR') %>')")
+        .done();
         
-            //select_mold.clone("All")
-            //    .appendTitle("select all elements that match")
+    select_mold.clone("All")
+        .tooltip("All elements that match the selection")
+        .appendTitle("select all elements that match")
+        .appendTitle(D3_TYPES.SELECTION.field)
+        .code("d3.selectAll('<%= block.getTitleValue('SELECTOR') %>')")
+        .done();
+    
+        
+    select_arg_mold.clone("selectAll")
+        .tooltip("All elements that match the selection")
+        .appendTitle("select all elements that match")
+        .appendTitle(D3_TYPES.SELECTION.field)
+        .appendInput(D3_TYPES.PARENT.field)
+        .code([
+                "<%= block.BG.valueToCode(block, 'PARENT', block.BG.ORDER_NONE) %>", 
+                ".selectAll('<%= block.getTitleValue('SELECTOR') %>')"
+            ].join("\n\t"))
+        .done();
+        /*
+        BL.d3_selectAll = {
+          // d3 Selection value.
+          init: function() {
+            this.appendTitle(new Blockly.FieldTextInput(''), 'TEXT');
+            this.appendInput('from', Blockly.INPUT_VALUE, 'PARENT', Selection);
+          }
+        };
+        
+        BJS.d3_selectAll = function() {
+            return join_none([
+                BJS.valueToCode(this, 'PARENT', BJS.ORDER_NONE),
+                ".selectAll('",
+                this.getTitleValue('TEXT'),
+                "')"
+            ]);
+        };
+        */
 })(b4);
