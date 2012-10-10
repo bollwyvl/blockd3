114da40aecbf525aa4ed0c2923ab4e9fecf57388
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2012-10-10 19:35:39 -0400

    adding submodule for gh-pages

--- fabfile.py
+++ fabfile.py
@@ -16,28 +16,10 @@ from fabric.api import local
 PROJECT_ROOT = local("echo $(git rev-parse --show-toplevel)")
 DEMO_LIB = os.path.join(PROJECT_ROOT, "demo", "lib")
 
-demo_deps = {
-    "blockly": {
-        "get": "svn export http://blockly.googlecode.com/svn/trunk/ %s",
-        "cleanup": "python %s/build.py"
-    },
-    "d3": {
-        "get": "git clone --depth 1 https://github.com/mbostock/d3.git %s",
-        "cleanup": "rm -rf %s/.git"
-    }
-}
 
-def update_demo_deps():
-    """
-    grab and document the newest version of the demo libs (blockly, d3)
-    """
-    for dep, cmds in demo_deps.items():
-        dep_path = os.path.join(DEMO_LIB, dep)
-        local("rm -rf %s" % dep_path)
-        local(cmds["get"] % dep_path)
-        if "cleanup" in cmds:
-            local(cmds["cleanup"] % dep_path)
-            
+def build():
+    pass
+
         
 def serve():
     import SimpleHTTPServer
