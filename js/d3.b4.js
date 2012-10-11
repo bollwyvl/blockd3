daa4e2d144594a4b08b17701fa0cc379955e827a
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2012-10-11 02:03:49 -0400

    field weirdness

--- js/d3.b4.js
+++ js/d3.b4.js
@@ -7,8 +7,14 @@ var D3_WIKI = "https://github.com/mbostock/d3/wiki/",
             field: b4.fields.text("SELECTOR")
                 .init("CSS-style selector")
         },
+        PARENT: {
+            id: "D3 Selection",
+            field: b4.input("PARENT")
+                .title("of selection")
+                .inputValue(true)
+        },
         SVG_ELEMENT: {
-            id: "D3 SVG entity",
+            id: String,
             field: b4.fields.choices("ELEMENT")
                 .title("SVG Element")
                 .init([
@@ -22,44 +28,38 @@ var D3_WIKI = "https://github.com/mbostock/d3/wiki/",
                     ["path", "path"]
                 ])
         },
-        PARENT: {
-            id: "D3 Parent Selection",
-            field: b4.input("PARENT")
-                .title("of selection")
-                .inputValue(true)
-        },
         ATTR_PROP: {
-            id: "D3 attribute",
+            id: String,
             field: b4.input("ATTR_PROP")
                 .inputValue(true)
                 .title("DOM attribute")
         },
         STYLE_PROP: {
-            id: "D3 CSS Style Property",
+            id: String,
             field: b4.input("STYLE_PROP")
                 .inputValue(true)
                 .title("CSS property")
         },
         VALUE: {
-            id: "D3 general calc value",
+            id: true,
             field: b4.input("VALUE")
                 .inputValue(true)
                 .title("to value")
         },
         KLASS: {
-            id: "D3 class",
+            id: String,
             field: b4.input("KLASS")
                 .inputValue(true)
                 .title("CSS class")
         },
         PROP: {
-            id: "D3 property",
+            id: String,
             field: b4.input("PROP")
                 .inputValue(true)
                 .title("property named")
         },
         ENTITY: {
-            id: "D3 entity",
+            id: String,
             field: b4.input("ENTITY")
                 .inputValue(true)
                 .title("tag named")
@@ -88,14 +88,14 @@ var d3_mold = b4.block()
 select_mold.clone("")
     .tooltip("The first element that matches the selector")
     .appendTitle(["select the first element that matches",
-        D3_TYPES.SELECTION.field])
+        D3_TYPES.SELECTION])
     .code("d3.select('<%= $.title('SELECTOR') %>')")
     .done();
         
 select_mold.clone("All")
     .tooltip("All elements that match the selection")
     .appendTitle(["select all elements that match",
-        D3_TYPES.SELECTION.field])
+        D3_TYPES.SELECTION])
     .code("d3.selectAll('<%= $.title('SELECTOR') %>')")
     .done();
     
@@ -103,8 +103,8 @@ select_mold.clone("All")
 select_arg_mold.clone("selectAll")
     .tooltip("All elements that match the selection")
     .appendTitle(["select all elements that match",
-        D3_TYPES.SELECTION.field])
-    .appendInput(D3_TYPES.PARENT.field)
+        D3_TYPES.SELECTION])
+    .appendInput(D3_TYPES.PARENT)
     .code([
             "<%= $.code('PARENT', 'd3.select(document)') %>", 
             ".selectAll('<%= $.title('SELECTOR') %>')"
@@ -114,7 +114,7 @@ select_arg_mold.clone("selectAll")
 select_mold.clone("enter")
     .tooltip("returns placeholders for missing elements")
     .appendTitle("enter")
-    .appendInput(D3_TYPES.PARENT.field)
+    .appendInput(D3_TYPES.PARENT)
     .code(["<%= $.code('PARENT') %>", 
             ".enter()"])
     .done();
@@ -126,13 +126,13 @@ var manip_mold = d3_mold.clone()
         .namespace("d3_"),
     get_mold = manip_mold.clone()
         .category("d3 getters")
-        .output(D3_TYPES.VALUE);
+        .output(null);
     
 manip_mold.clone("style") 
     .appendTitle("set d3.style")
-    .appendInput([D3_TYPES.STYLE_PROP.field,
-        D3_TYPES.PARENT.field,
-        D3_TYPES.VALUE.field])
+    .appendInput([D3_TYPES.STYLE_PROP,
+        D3_TYPES.PARENT,
+        D3_TYPES.VALUE])
     .code(["<%= $.code('PARENT', 'd3.select(\"circle\")') %>", 
         ".style(<%= $.code('STYLE_PROP') %>, <%= $.code('VALUE') %>)"])
     .done();
@@ -140,8 +140,8 @@ manip_mold.clone("style")
 get_mold.clone("style_get") 
     .appendTitle("get d3.style")
     .output(D3_TYPES.SELECTION)
-    .appendInput([D3_TYPES.STYLE_PROP.field,
-        D3_TYPES.PARENT.field])
+    .appendInput([D3_TYPES.STYLE_PROP,
+        D3_TYPES.PARENT])
     .code(["<%= $.code('PARENT', 'd3.select(\"circle\")') %>", 
         ".style(<%= $.code('STYLE_PROP') %>)"])
     .done();
@@ -149,9 +149,9 @@ get_mold.clone("style_get")
 manip_mold.clone("attr")
     .tooltip("Set the SVG attribute")
     .appendTitle("d3.attr")
-    .appendInput([D3_TYPES.ATTR_PROP.field,
-        D3_TYPES.PARENT.field,
-        D3_TYPES.VALUE.field])
+    .appendInput([D3_TYPES.ATTR_PROP,
+        D3_TYPES.PARENT,
+        D3_TYPES.VALUE])
     .code(["<%= $.code('PARENT') %>", 
         ".attr(<%= $.code('ATTR_PROP') %>, <%= $.code('VALUE') %>)"])
     .done();
@@ -159,8 +159,8 @@ manip_mold.clone("attr")
 manip_mold.clone("data")
     .tooltip("Set the data for a selection")
     .appendTitle("d3.data")
-    .appendInput([D3_TYPES.PARENT.field,
-        D3_TYPES.VALUE.field])
+    .appendInput([D3_TYPES.PARENT,
+        D3_TYPES.VALUE])
     .code(["<%= $.code('PARENT') %>", 
         ".data(<%= $.code('VALUE') %>)"])
     .done();
@@ -168,9 +168,9 @@ manip_mold.clone("data")
 manip_mold.clone("classed")
     .tooltip("Set the class of a selection")
     .appendTitle("d3.class")
-    .appendInput([D3_TYPES.KLASS.field,
-        D3_TYPES.PARENT.field,
-        D3_TYPES.VALUE.field])
+    .appendInput([D3_TYPES.KLASS,
+        D3_TYPES.PARENT,
+        D3_TYPES.VALUE])
     .code(["<%= $.code('PARENT') %>", 
         ".classed(<%= $.code('KLASS') %>, <%= $.code('VALUE') %>)"])
     .done();
@@ -178,9 +178,9 @@ manip_mold.clone("classed")
 manip_mold.clone("property")
     .tooltip("get or set raw properties")
     .appendTitle("d3.property")
-    .appendInput([D3_TYPES.PARENT.field,
-        D3_TYPES.PROP.field,
-        D3_TYPES.VALUE.field])
+    .appendInput([D3_TYPES.PARENT,
+        D3_TYPES.PROP,
+        D3_TYPES.VALUE])
     .code(["<%= $.code('PARENT') %>", 
         ".property(<%= $.code('PROP') %>, <%= $.code('VALUE') %>)"])
     .done();
@@ -188,8 +188,8 @@ manip_mold.clone("property")
 manip_mold.clone("text")
     .tooltip("get or set text")
     .appendTitle("d3.text")
-    .appendInput([D3_TYPES.PARENT.field,
-        D3_TYPES.VALUE.field])
+    .appendInput([D3_TYPES.PARENT,
+        D3_TYPES.VALUE])
     .code(["<%= $.code('PARENT') %>", 
         ".text(<%= $.code('VALUE') %>)"])
     .done();
@@ -197,8 +197,8 @@ manip_mold.clone("text")
 manip_mold.clone("append")
     .tooltip("get or set html")
     .appendTitle("d3.append")
-    .appendInput([D3_TYPES.PARENT.field,
-        D3_TYPES.ENTITY.field])
+    .appendInput([D3_TYPES.PARENT,
+        D3_TYPES.ENTITY])
     .code(["<%= $.code('PARENT') %>", 
         ".append(<%= $.code('ENTITY') %>)"])
     .done();
@@ -206,8 +206,8 @@ manip_mold.clone("append")
 manip_mold.clone("insert")
     .tooltip("create and insert new elements before existing elements")
     .appendTitle("d3.insert")
-    .appendInput([D3_TYPES.PARENT.field,
-        D3_TYPES.ENTITY.field])
+    .appendInput([D3_TYPES.PARENT,
+        D3_TYPES.ENTITY])
     .code(["<%= $.code('PARENT') %>", 
         ".insert(<%= $.code('ENTITY') %>)"])
     .done();
@@ -215,8 +215,8 @@ manip_mold.clone("insert")
 manip_mold.clone("remove")
     .tooltip("remove elements from the document")
     .appendTitle("d3.remove")
-    .appendInput([D3_TYPES.PARENT.field,
-        D3_TYPES.ENTITY.field])
+    .appendInput([D3_TYPES.PARENT,
+        D3_TYPES.ENTITY])
     .code(["<%= $.code('PARENT') %>", 
         ".remove())"])
     .done();
@@ -228,11 +228,11 @@ var svg_mold = b4.block()
     .helpUrlTemplate(D3_WIKI+"SVG-Shapes#wiki-")
     .namespace("d3_svg_elements_")
     .colour("orange")
-    .output(D3_TYPES.SVG_ELEMENT);
+    .output(null);
 
 svg_mold.clone("element")
     .tooltip("an SVG element")
-    .appendTitle(["SVG", D3_TYPES.SVG_ELEMENT.field])
+    .appendTitle(["SVG", D3_TYPES.SVG_ELEMENT])
     .code("<%= $.title('ELEMENT') %>")
     .done();
 
