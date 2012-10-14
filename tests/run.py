c315cc58da2f0dca9318c2716e673687248f0ebb
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2012-10-14 03:16:10 -0400

    almost working

--- tests/run.py
+++ tests/run.py
@@ -16,15 +16,16 @@ class GhostTest(GhostTestCase):
     port = PORT
     display = False
     log_level = logging.INFO
+    wait_ghost = Ghost(wait_timeout=10)
 
     @classmethod
     def create_app(cls):
         return app
 
     def test_open(self):
-        page, resources = self.ghost.open(base_url)
+        page, resources = self.wait_ghost.open(base_url)
         self.assertEqual(page.url, base_url)
-        self.assertTrue("Test page" in self.ghost.content)
+        self.assertTrue("Test page" in self.wait_ghost.content)
 
 if __name__ == '__main__':
     unittest.main()
