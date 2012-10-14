f1d546bb1ecce38cb6f85b253fb94c4098db3565
Nicholas Bollweg (Nick) <nick.bollweg@gmail.com>
2012-10-14 03:21:06 -0400

    subclassing ghost test

--- tests/run.py
+++ tests/run.py
@@ -12,20 +12,28 @@ PORT = 5000
 
 base_url = 'http://localhost:%s/' % PORT
 
-class GhostTest(GhostTestCase):
+class Blockd3GhostTest(GhostTestCase):
     port = PORT
     display = False
     log_level = logging.INFO
-    wait_ghost = Ghost(wait_timeout=10)
+
+    def __new__(cls, *args, **kwargs):
+        """Creates Ghost instance."""
+        if not hasattr(cls, 'ghost'):
+            cls.ghost = Ghost(display=cls.display,
+                wait_timeout=10,
+                viewport_size=cls.viewport_size,
+                log_level=cls.log_level)
+        return super(Blockd3GhostTest, cls).__new__(cls, *args, **kwargs)
 
     @classmethod
     def create_app(cls):
         return app
 
     def test_open(self):
-        page, resources = self.wait_ghost.open(base_url)
+        page, resources = self.ghost.open(base_url)
         self.assertEqual(page.url, base_url)
-        self.assertTrue("Test page" in self.wait_ghost.content)
+        self.assertTrue("Test page" in self.ghost.content)
 
 if __name__ == '__main__':
     unittest.main()
